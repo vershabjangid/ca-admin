36ce12c01a55ba0215f2705308d5ed840d3a5c30
mohit <mohitgehlot9656@example.com>
2024-05-06 17:12:39 +0530

    login design complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,25 @@
-import { faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
 export function Login() {
+
+  let [password_show, password_hide] = useState('password')
+  let password_icons_chenge = () => {
+    password_hide('text');
+  }
+
+  let password_sow = ()=>{
+    password_hide('password');
+  }
+
   return (
     <>
-      <div className="login pt-5">
-        <Row className="main_login mx-auto rounded-4 mt-4">
-          <Col className="col-6 p-5 mt-4">
-            <div className="login_logo ps-5">
+      <div className="login p-1">
+        <Row className="main_login mx-auto   rounded-4">
+          <Col className="col-lg-6 col-12 p-lg-4 pt-5    mt-4">
+            <div className="login_logo">
               <img
                 width={"100px"}
                 height={"90px"}
@@ -18,10 +28,10 @@ export function Login() {
               />
             </div>
             <div className="fw-bold">
-              <h2 className="text-capitalize  ps-5 pt-4 fs-1">
+              <h2 className="text-capitalize   pt-4 fs-1">
                 wellcome back!
               </h2>
-              <p className="text-capitalize ps-5 ms-1">
+              <p className="text-capitalize ms-1">
                 please enter log in detals below
               </p>
             </div>
@@ -30,37 +40,47 @@ export function Login() {
                 <input
                   type="Email"
                   placeholder="Email "
-                  className="d-block my-3 border-0  ps-2 rounded  py-4 ms-5 w-75 "
+                  className="d-block my-3 border-0  ps-2 rounded  py-lg-4 py-2  w-100"
                 />
                 <div className=" position-relative">
                   <input
-                    type="password"
+                    type={password_show}
                     placeholder="Password "
-                    className="d-block my-3 border-0  ps-2 rounded  py-4 ms-5 w-75 "
+                    className="d-block my-3 border-0  ps-2 rounded   py-lg-4 py-2 w-100 "
                   />
-                  <FontAwesomeIcon
-                    icon={faEyeSlash}
-                    className="fs-6 position-absolute icon_password"
-                  />
+                  {
+                   
+                      (password_show  == 'password')
+                      ?
+                      <FontAwesomeIcon
+                        icon={faEyeSlash}
+                        className="fs-6 position-absolute icon_password"
+                        onClick={() => password_icons_chenge()}
+                      />
+                      :
+                      <FontAwesomeIcon icon={faEye} onClick={() =>password_sow('passsword')} className="fs-6 position-absolute icon_password" />
+                  }
+
                 </div>
-                <div className="text-end pe-5">
-                    <p className="fs-5 fw-bold pe-5 me-4 password_forget">
-                        Forget Password ? 
-                    </p>
+                <div className="text-end">
+                  <p className="fs-5 fw-bold password_forget">
+                    Forget Password ?
+                  </p>
                 </div>
-                <div className="ps-5 ">
-                    <button className="btn text-light fw-bold fs-4 p-3  text-center submit_login">
-                        Submit
-                    </button>
+                <div className="">
+                  <button className="btn text-light fw-bold fs-4 p-lg-3 p-2 text-center submit_login">
+                    Submit
+                  </button>
                 </div>
-               
               </form>
             </div>
           </Col>
-          <Col className="col-6 mt-5 pt-4">
+          <Col className="col-lg-6 d-none d-lg-block  mt-4 pt-4">
             <img
               src="https://i.vimeocdn.com/video/1447123850-31d8db602ef43412a9b4731cf141f6a2c56e17609a1cb13f00b3e4b864dd2e7b-d_640?f=webp"
               alt=""
+              width={"100%"}
+              height={"80%"}
             />
           </Col>
         </Row>
